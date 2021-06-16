8ec7850599d25acd61eef8333213edfb33d1b951
aojie654 <aojie654@live.cn>
2021-06-16 21:54:58 +0800

    Add config support for ddns_google

--- ddns_google.py
+++ ddns_google.py
@@ -1,39 +1,42 @@
-import os
-import requests
+import configparser
 import datetime
-import dns.resolver
 import logging
+import os
 from datetime import datetime
 
+import dns.resolver
+import requests
 
-# Log path for Windows, macOS, Linux
-# log_folder = "D:\\tmp\\"
-# log_folder = "~/log/ddns/"
-log_folder = "/var/log/ddns/"
+# Root path
+path_root = os.path.dirname(__file__)
+
+# Log Path in path_root/log/
+path_log_dir = os.path.join(path_root, "log")
+filename_log = "ddns_google_{0:%Y%m%d}.log".format(datetime.now())
+path_log = os.path.join(path_log_dir, filename_log)
+
 # Create log folder if not exist
-if not os.path.exists(log_folder):
-    os.mkdir(log_folder)
-
-log_filename = "ddns_google_{1:%Y%m%d}".format(datetime.now())
-log_path = os.path.join(log_folder, log_filename)
+if not os.path.exists(path_log_dir):
+    os.mkdir(path_log_dir)
 
 # Set log format
-logging.basicConfig(format="%(asctime)s; %(levelname)s; %(message)s", filename=log_filename, level=logging.INFO)
+logging.basicConfig(format="%(asctime)s; %(levelname)s; %(message)s", filename=path_log, level=logging.INFO)
 
-# Proxy settings
-proxies_set = {
-    "http": "Your HTTP Proxy",
-    "https": "Your HTTPS Proxy",
-}
+# Config path is path_root/.config
+filename_config = "google.ini"
+path_config_dir = os.path.join(path_root, "configs")
+path_config = os.path.join(path_config_dir, filename_config)
+log_content = "Config path is: {0}".format(path_config)
+logging.info(log_content)
 
-# Google Domains hostnames and credentials
-update_lists = [
-    {
-        "hostname": "Your Google DDNS Hostname",
-        "username": "Your Google DDNS Username",
-        "password": "Your Google DDNS Password"
-    },
-]
+# Load config
+config = configparser.ConfigParser()
+config.read(path_config)
+
+# Load Proxy
+proxies_set = config._sections["proxy"]
+log_content = "Proxy is: {0}".format(proxies_set)
+logging.info(log_content)
 
 
 def get_global_ip():
@@ -43,22 +46,22 @@ def get_global_ip():
     Return: Global IP
     """
 
-    # URL to get IPv4 address
-    url_ip4 = "https://v4.ipv6-test.com/api/myip.php"
     # Get current global IPv4
-    # Use 1.1.1.1 as result IPv4 address for debug.
-    # result_ip4_cur = "1.1.1.1"
-    result_ip4_cur = requests.get(url_ip4).text.replace("\n", "")
-
+    # url_ip4 = "https://v4.ipv6-test.com/api/myip.php"
+    # result_ip4_cur = "127.0.0.1"
+    # result_ip4_cur = requests.get(url_ip4, timeout = 5).text.replace("\n", "")
+    # Use 127.0.0.1 as result IPv4 address for debug.
     # URL to get IPv4 address
-    # url_ip6 = "https://v6.ipv6-test.com/api/myip.php"
+
     # Get current global IPv6
-    # Use fe80::1 as result IPv6 address for debug.
-    # result_ip6_cur = "fe80::1"
-    # result_ip6_cur = requests.get(url_ip6).text.replace("\n", "")
+    # URL to get IPv6 address
+    # url_ip6 = "https://v6.ipv6-test.com/api/myip.php"
+    # result_ip6_cur = requests.get(url_ip6, timeout=5).text.replace("\n", "")
+    # Use ::1 as result IPv6 address for debug.
+    result_ip6_cur = "::1"
 
     # Change result here
-    result_ip_cur = result_ip4_cur
+    result_ip_cur = result_ip6_cur
     # result_ip_cur = result_ip6_cur
 
     # Log current IP
@@ -80,14 +83,14 @@ def get_dns_ip(hostname):
     resolver_tmp.nameservers = ["8.8.8.8", "8.8.4.4"]
 
     # IPv4 query
-    result_ip4_dns = resolver_tmp.query(hostname, 'A').rrset[0].address
+    # result_ip4_dns = resolver_tmp.resolve(hostname, 'A').rrset[0].address
 
     # IPv6 query
-    # result_ip6_dns = resolver_tmp.query(hostname, 'AAAA').rrset[0].address
+    result_ip6_dns = resolver_tmp.resolve(hostname, 'AAAA').rrset[0].address
 
     # Change the result here
-    result_ip_dns = result_ip4_dns
-    # result_ip_dns = result_ip6_dns
+    # result_ip_dns = result_ip4_dns
+    result_ip_dns = result_ip6_dns
 
     # Log current IP
     if result_ip_dns != "":
@@ -105,15 +108,15 @@ def log_generator(hostname, ip_cur, ip_dns, result_update):
     Input: Hostname, current ip, DNS ip, update result
     Return: Hostname: {{hostname}}, current IP: {{ip_cur}}, DNS IP: {{ip_dns}}, update result: {{result_update}}
     """
-    
+
     # Define log tamplate
     log_content = "Hostname: " + hostname + ", current IP: " + ip_cur + \
-        ", DNS IP: " + ip_dns + ", update result: " + result_update + "\n"
+        ", DNS IP: " + ip_dns + ", update result: " + result_update
 
     return log_content
 
 
-def dns_update_step(update_meta, result_ip_cur):
+def dns_update_step(hostname_tmp, result_ip_cur):
     """
     Update DNS record.
     Input: IP information, current ip
@@ -121,9 +124,9 @@ def dns_update_step(update_meta, result_ip_cur):
     """
 
     # Get username, password, hostname, current IP
-    username = update_meta["username"]
-    password = update_meta["password"]
-    hostname = update_meta["hostname"]
+    hostname = hostname_tmp
+    username = config[hostname_tmp]["username"]
+    password = config[hostname_tmp]["password"]
     ip_cur = result_ip_cur
 
     # Initial data using to post
@@ -144,7 +147,7 @@ def dns_update_step(update_meta, result_ip_cur):
         try:
             # Update DNS record with HTTP POST
             url_update = "https://{0}:{1}@domains.google.com/nic/update".format(username, password)
-            result_update = requests.post(url_update, data=data_post, proxies=proxies_set).text
+            result_update = requests.request(method="POST", url=url_update, data=data_post, proxies=proxies_set, timeout=5).text
         except Exception as exception_tmp:
             result_update = "Error occured: {0}".format(exception_tmp)
     else:
@@ -152,7 +155,7 @@ def dns_update_step(update_meta, result_ip_cur):
         result_update = "No need update."
     # Log result
     log_content = log_generator(hostname, ip_cur, ip_dns, result_update)
-    logging.INFO(log_content)
+    logging.info(log_content)
 
     return
 
@@ -165,6 +168,10 @@ def testing():
 
 
 if __name__ == "__main__":
+
+    # Log script start
+    log_content = "{0:=^50}".format(" DDNS script start ")
+    logging.info(log_content)
     # Using your testing function here
     # testing()
 
@@ -173,6 +180,13 @@ if __name__ == "__main__":
     log_content = "Current IP is: {0}".format(result_ip_cur)
 
     # Update each hostname in hostnames list
-    for update_meta in update_lists:
-        dns_update_step(update_meta, result_ip_cur)
+    for section_tmp in config.sections():
+        if section_tmp != "proxy":
+            dns_update_step(section_tmp, result_ip_cur)
+        else:
+            continue
     pass
+
+    # Log script end
+    log_content = "{0:=^50}\n".format(" DDNS script End ")
+    logging.info(log_content)
